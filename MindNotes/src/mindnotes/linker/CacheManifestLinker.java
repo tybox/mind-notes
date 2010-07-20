cddbea7d55757591f8983b031cd4413a68cf530c
dstozek <dstozek@7d4a0afc-fc9d-9282-b08a-abe350bbcb43>
2010-07-20 17:23:42 +0000

    modified CacheManifestLinker to create a complete cache manifest file.

--- MindNotes/src/mindnotes/linker/CacheManifestLinker.java
+++ MindNotes/src/mindnotes/linker/CacheManifestLinker.java
@@ -1,5 +1,7 @@
 package mindnotes.linker;
 
+import java.io.File;
+
 import com.google.gwt.core.ext.LinkerContext;
 import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.UnableToCompleteException;
@@ -14,10 +16,9 @@ import com.google.gwt.core.ext.linker.LinkerOrder.Order;
  * A simple linker that generates a cache manifest file based on emitted
  * compilation artifacts.
  * 
- * In the current implementation, the linker generates a cache.manifest.template
- * file; This file does not include static files that might be needed to be
- * included in the manifest. You have to update the /cache.manifest file
- * manually.
+ * In the current implementation, the linker generates a cache.manifest file in
+ * the war/mindnotes directory. It contains references to all permutation files
+ * and to all static files.
  * 
  * Inspiration/reference: a talk on Google I/O 2010
  * "GWT Linkers target HTML5 WebWorkers & more"
@@ -37,25 +38,78 @@ public class CacheManifestLinker extends AbstractLinker {
 	@Override
 	public ArtifactSet link(TreeLogger logger, LinkerContext context,
 			ArtifactSet artifacts) throws UnableToCompleteException {
+
 		ArtifactSet as = new ArtifactSet(artifacts);
 
+		// look in the war directory for files that should be included
+		File dir = new File("war/");
+		if (dir.isDirectory()) {
+
+			EmittedArtifact manifest = emitManifest(logger, context, as, dir);
+			as.add(manifest);
+
+		}
+
+		return as;
+	}
+
+	private EmittedArtifact emitManifest(TreeLogger logger,
+			LinkerContext context, ArtifactSet as, File dir)
+			throws UnableToCompleteException {
 		StringBuilder sb = new StringBuilder("CACHE MANIFEST\n");
 
+		sb.append("# static files\n");
+		for (File f : dir.listFiles()) {
+			if (f.isDirectory()) {
+				if (!(f.getName().equals(context.getModuleName())
+						|| f.getName().equals("WEB-INF") || f.getName()
+						.startsWith("."))) {
+					appendFiles(dir.getPath(), "../", f.getName(), sb);
+				}
+			} else {
+				sb.append("../" + f.getName() + "\n");
+			}
+		}
+
+		sb.append("# generated files\n");
 		for (EmittedArtifact ea : as.find(EmittedArtifact.class)) {
 			if (ea.isPrivate())
 				continue;
 			if (ea.getPartialPath().endsWith(".gwt.rpc"))
 				continue; // ignore .gwt.rpc files; they are not served to the
-							// client anyway
-			// XXX or are they?
-			sb.append(context.getModuleName() + "/");
+			// client anyway
 			sb.append(ea.getPartialPath() + "\n");
 
 		}
 		EmittedArtifact manifest = emitString(logger, sb.toString(),
-				"cache.manifest.template");
-		as.add(manifest);
+				"cache.manifest");
+		return manifest;
+	}
 
-		return as;
+	private void appendFiles(String parentPath, String appendPath, String dir,
+			StringBuilder sb) {
+		File dirfile = new File(parentPath, dir);
+		if (!dirfile.isDirectory())
+			return;
+		for (File f : dirfile.listFiles()) {
+
+			// ignore hidden files
+			if (f.getName().startsWith("."))
+				continue;
+
+			File appendFile = new File(appendPath, dir);
+
+			if (f.isDirectory()) {
+				// recursive call
+				appendFiles(dirfile.getPath(), appendFile.getPath(), f
+						.getName(), sb);
+
+			} else {
+				// append a new file declaration in the manifest
+				sb.append(new File(appendFile, f.getName()) + "\n");
+			}
+
+		}
+
 	}
 }
